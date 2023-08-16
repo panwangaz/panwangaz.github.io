c52869803deb9e0a035d3f470e767167140592b5
panwang <panwang725@gmail.com>
2023-08-16 09:56:01 +0800

    update files in 2023.0816

--- _posts/deeplearning/attention.py
+++ _posts/deeplearning/attention.py
@@ -1,62 +1,42 @@
 import torch
 import torch.nn as nn
-import numpy as np
 import torch.nn.functional as F
 
-
 class MultiHeadAttention(nn.Module):
-    '''
-    input:
-        query --- [N, T_q, query_dim]
-        key --- [N, T_k, key_dim]
-        mask --- [N, T_k]
-        T_q 相当于是图像中的 H*W
-    output:
-        out --- [N, T_q, embed_dim]
-        scores -- [h, N, T_q, T_k]
-    '''
-
     def __init__(self, query_dim, key_dim, embed_dim, num_heads):
-
-        super().__init__()
+        super(MultiHeadAttention, self).__init__()
+        self.query_dim = query_dim
+        self.key_dim = key_dim
         self.embed_dim = embed_dim
         self.num_heads = num_heads
-        self.key_dim = key_dim
-
-        self.W_query = nn.Linear(query_dim, embed_dim, bias=False)
-        self.W_key = nn.Linear(key_dim, embed_dim, bias=False)
-        self.W_value = nn.Linear(key_dim, embed_dim, bias=False)
+        self.query_proj = nn.Linear(query_dim, embed_dim, bias=False)
+        self.key_proj = nn.Linear(key_dim, embed_dim, bias=False)
+        self.value_proj = nn.Linear(key_dim, embed_dim, bias=False)
 
     def forward(self, query, key, mask=None):
-        querys = self.W_query(query)  # [N, T_q, embed_dim]
-        keys = self.W_key(key)  # [N, T_k, embed_dim]
-        values = self.W_value(key)
+        querys = self.query_proj(query)
+        keys = self.key_proj(key)
+        values = self.value_proj(key)
 
         assert self.embed_dim % self.num_heads == 0
         split_size = self.embed_dim // self.num_heads
-        querys = torch.stack(torch.split(querys, split_size, dim=2), dim=0)  # [h, N, T_q, embed_dim/h]
-        keys = torch.stack(torch.split(keys, split_size, dim=2), dim=0)  # [h, N, T_k, embed_dim/h]
-        values = torch.stack(torch.split(values, split_size, dim=2), dim=0)  # [h, N, T_k, embed_dim/h]
+        querys = torch.stack(querys.split(split_size, dim=2), dim=0)
+        keys = torch.stack(keys.split(split_size, dim=2), dim=0)
+        values = torch.stack(values.split(split_size, dim=2), dim=0)
 
-        ## score = softmax(QK^T / (d_k ** 0.5))
-        scores = torch.matmul(querys, keys.transpose(2, 3))  # [h, N, T_q, T_k]
-        scores = scores / (self.key_dim ** 0.5)
+        weights = torch.matmul(querys, keys.transpose(2, 3))
+        weights = weights / (self.key_dim ** 0.5)
 
-        ## mask
         if mask is not None:
-            ## mask:  [N, T_k] --> [h, N, T_q, T_k]
-            mask = mask.unsqueeze(1).unsqueeze(0).repeat(self.num_heads,1,querys.shape[2],1)
-            # 将 mask 中为 1 的元素所在的索引在 score 中替换成 -np.inf，经过 softmax 之后这部分的值会变成 0
-            # 相当于这部分就不进行 attention 的计算 （ np.exp(-np.inf) = 0 ）
-            scores = scores.masked_fill(mask, -np.inf)
-        scores = F.softmax(scores, dim=3)
+            mask = mask.unsqueeze(1).unsqueeze(0).repeat(self.num_heads, 1, querys.shape[2], 1)
+            weights = weights.masked_fill(mask, -torch.inf)
+        weights = F.softmax(weights, dim=3)
 
-        ## out = score * V
-        out = torch.matmul(scores, values)  # [h, N, T_q, embed_dim/h]
-        out = torch.cat(torch.split(out, 1, dim=0), dim=3).squeeze(0)  # [N, T_q, embed_dim]
+        out = torch.matmul(weights, values)
+        out = torch.cat(out.split(1, dim=0), dim=3).squeeze(0)
 
-        return out, scores
-
+        return out, weights
+    
 attention = MultiHeadAttention(256,512,256,16)
 
 ## 输入
