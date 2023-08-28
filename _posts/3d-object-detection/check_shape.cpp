105ee0b118920e66117be2bbaabb71652a60c639
panwang <panwang725@gmail.com>
2023-08-28 17:16:17 +0800

    update files in 2023.08.24

--- _posts/3d-object-detection/check_shape.cpp
+++ _posts/3d-object-detection/check_shape.cpp
@@ -12,37 +12,30 @@ int crossProduct(const Point& p1, const Point& p2, const Point& p3) {
     return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
 }
 
-// 判断多边形是否为凸边形
 bool isConvexPolygon(const vector<Point>& polygon) {
-    int n = polygon.size();
-
-    // 遍历每个顶点
-    for (int i = 0; i < n; i++) {
-        int cp = crossProduct(polygon[i], polygon[(i + 1) % n], polygon[(i + 2) % n]);
-
+    for (int i = 0; i < polygon.size(); i++) {
+        int cur = crossProduct(polygon[(i + 1) % n], polygon[i], polygon[(i + 2) % n]);
         // 如果相邻边的转向不一致，多边形不是凸边形
-        if (cp * crossProduct(polygon[i], polygon[(i + 1) % n], polygon[(i + 3) % n]) < 0) {
+        if (cur < 0) {
             return false;
         }
     }
-
-    // 所有相邻边的转向一致，多边形是凸边形
     return true;
 }
 
 int main() {
-    int n;
-    while (cin >> n && n > 0) {
-        vector<Point> polygon(n);
-        for (int i = 0; i < n; i++) {
-            cin >> polygon[i].x >> polygon[i].y;
-        }
+    int n = 3;
+    vector<vector<int>> data{{1, 2}, {2, 3}, {3, 4}};
+    vector<Point> polygon(n);
+    for (int i = 0; i < n; i++) {
+        polygon[i].x = data[i][0];
+        polygon[i].y = data[i][1];
+    }
 
-        if (isConvexPolygon(polygon)) {
-            cout << "convex" << endl;
-        } else {
-            cout << "concave" << endl;
-        }
+    if (isConvexPolygon(polygon)) {
+        cout << "convex" << endl;
+    } else {
+        cout << "concave" << endl;
     }
 
     return 0;
